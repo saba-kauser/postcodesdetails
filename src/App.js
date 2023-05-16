060284746f2ffd03c97f69f9533399967f46e0f8
Saba Kauser <sabakauser411@gmail.com>
2023-05-16 19:00:24 +0100

    postcode API response display and tables for nearest postcodes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Link,Route,Routes,BrowserRouter as Router} from 'react-router-dom';
+import {Link,BrowserRouter as Router} from 'react-router-dom';
 import DisplayPostCodes from './PostcodeData/DisplayPostcodes';
 
 
@@ -8,10 +8,11 @@ function App() {
   const [enteredPostCode, setEnteredPostCode] = useState('');
   const[errorMessage,setErrorMessage]=useState('');
   
-
   const checkEnteredPostCode=()=>{
     if(enteredPostCode==='')
-    setErrorMessage('Please enter a valid postcode !')
+   {
+    setErrorMessage('empty postcode !')
+   }
   }
 
   return (
@@ -21,17 +22,17 @@ function App() {
       <h1 className='text-center mb-4 mt-4 bg-red bg-light'>Lookup Postcodes</h1>
         <div className='form component'>
           <h4>Please enter a postcode</h4>
-          <form className="input-group mb-3 "> 
-            <input className="" type='text' placeholder='CB4 0GF' value={enteredPostCode} onChange={(e) => setEnteredPostCode(e.target.value)} required="required"/>
+          <form className="input-group mb-3"> 
+            <input className="" type='text' placeholder='CB4 0GF' value={enteredPostCode} onChange={(e)=>setEnteredPostCode(e.target.value)} required="required"/>
              <div>
                 <Link to={`/${enteredPostCode}`} className="btn btn-outline-secondary ml-1" onClick={checkEnteredPostCode}>Submit</Link>
             </div>
             <div className="error text-danger ml-5 p-1 mt-1">{errorMessage}</div>
           </form>
         </div>
-          <Routes>
-          <Route path='/:postcode' render={(props) => <DisplayPostCodes {...props} errorMessage={setErrorMessage} />}/>
-          </Routes>
+         <div>
+          <DisplayPostCodes errorMessage={setErrorMessage}/>
+         </div>
       </div>
     </div>
     </Router>
--- src/PostcodeData/DisplayPostcodes.js
+++ src/PostcodeData/DisplayPostcodes.js
@@ -1,23 +1,68 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-import { getPostCodes } from '../postcodeAPI/postcodeAPI'
+import { getPostCodes,postcodeValidation } from '../postcodeAPI/postcodeAPI'
 
 function DisplayPostCodes(props) {
 
-    const postcode=props.match.params.postcode
     const [postCodeResult,setPostCodeResult]=useState({});
+    // const [nearestPostCode,setNearestPostCode]=useState([]);
+    const location=useLocation()
 
-useEffect(()=>{
-        setPostCodeResult(getPostCodes(postcode))
-},[postcode])
-
-console.log(postCodeResult)
+    useEffect(()=>{
+        const postcode=location.pathname.substring(1)
+         const requestPostcodeData=async(postcode)=>{
+            const postcodeIsValid=await postcodeValidation(postcode)
+            if (postcodeIsValid.result === true) {
+                const postcodes = await getPostCodes(postcode);
+                if(postcodes.status===200){
+                    setPostCodeResult({
+                            postcode: postcodes.result.postcode,
+                            country: postcodes.result.country,
+                            region: postcodes.result.region,
+                    })
+                }
+            }   
+            else{
+                props.errorMessage('Please enter a postcode that is valid')
+            }
+    }
+    
+    if (postcode) {
+        requestPostcodeData(postcode);
+    }
+},[location,props])
 
 return(
         <div>
             <div className='postcoderesults'>
-              <h5>Country: <span className='resultValue'>{postCodeResult.country}</span></h5>
-              <h5>Region: <span className='resultValue'>{postCodeResult.region}</span></h5>
+              <h3 className="bg-light mt-3">Postcode Details</h3>
+              <h6>Country: <span className='resultValue text-success'>{postCodeResult.country}</span></h6>
+              <h6>Region: <span className='resultValue text-success'>{postCodeResult.region}</span></h6>
+            </div>
+
+            <div>
+              <h3 className="bg-light">Nearest Postcode Details </h3>
+                <table className="table table-striped bg-grey">
+                        <thead> 
+                            <tr>
+                            <th scope="col">Postcode</th>
+                            <th scope="col">Country</th>
+                            <th scope="col">Region</th>
+                            </tr>
+                        </thead>
+                        {/* <tbody>
+                            {nearestResults.map((result, i) => {
+                            return (
+                                <tr key={i}>
+                                <th scope="row">{result.postcode}</th>
+                                <td>{result.country}</td>
+                                <td>{result.region}</td>
+                            </tr>
+                            )
+                            })}
+                        </tbody> */}
+                </table>
             </div>
       </div>
     )
