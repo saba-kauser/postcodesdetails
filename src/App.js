a15fb87af8a427fdd051e2cddb464fdeb929fb5e
Saba Kauser <sabakauser411@gmail.com>
2023-05-16 13:43:57 +0100

    error message , background and header

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import {Link, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
-  const [postcodeValue, setpostcodeValue] = useState('');
+  const [enteredPostCode, setenteredPostCode] = useState('');
+  const[errorMessage,seterrorMessage]=useState('');
+  
 
-  console.log(postcodeValue)
+  const checkEnteredPostCode=()=>{
+    if(enteredPostCode==='')
+    seterrorMessage('Please enter a valid postcode !')
+  }
+
   return (
     <Router>
     <div className="App">
-      <div className='container'>
-        <div className='form'>
-          <h3>Enter Postcode</h3>
-          <form className="input-group mb-3"> 
-            <input className="" type='text' placeholder='CB4 0GF' value={postcodeValue} onChange={(e) => setpostcodeValue(e.target.value)} required="required"/>
+      <div className='container-fluid'> 
+      <h1 className='text-center mb-4 mt-4 bg-red bg-light'>Lookup Postcodes</h1>
+        <div className='form form-container'>
+          <h4>Please enter a postcode</h4>
+          <form className="input-group mb-3 "> 
+            <input className="" type='text' placeholder='CB4 0GF' value={enteredPostCode} onChange={(e) => setenteredPostCode(e.target.value)} required="required"/>
              <div>
-                <Link to={`/${postcodeValue}`} className="btn btn-outline-secondary">Submit</Link>
+                <Link to={`/${enteredPostCode}`} className="btn btn-outline-secondary ml-1" onClick={checkEnteredPostCode}>Submit</Link>
             </div>
+            <div className="error text-danger ml-5 p-1 mt-1">{errorMessage}</div>
           </form>
         </div>
       </div>
